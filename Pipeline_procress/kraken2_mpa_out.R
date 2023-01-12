63d23b5e35ec0ea86373fb5dbf378110a143301a
ianyen77 <96662135+ianyen77@users.noreply.github.com>
2023-01-12 18:56:08 +0800

    1

--- Pipeline_procress/kraken2_mpa_out.R
+++ Pipeline_procress/kraken2_mpa_out.R
@@ -3,7 +3,7 @@ library("openxlsx")
 library("tidyverse")
 library("ggplot2")
 #記得輸入前要先把mpa的樣品名稱改掉
-taxon_mpa_format<-read.xlsx(xlsxFile = "C://Users/USER/Desktop/kraken2_mpa_combine.xlsx",sheet=1,colNames = T)
+taxon_mpa_format<-read.xlsx(xlsxFile = "C://Users/USER/Desktop/lab/實驗/Metagenomic in DWDS/DATA/Taxa/kraken2_mpaout.xlsx",sheet=1,colNames = T)
 taxon_mpa_format<-taxon_mpa_format%>%
   filter(str_detect(`#Classification`,pattern="k__Bacteria"))
 
@@ -14,47 +14,47 @@ write.xlsx(taxon_mpa1,"C://Users/USER/Desktop/taxon_adjust_mpa.xlsx")
 #在這之後我們需要去excel手動調整將正確的分類放在正確的column
 #所有的column下面的分類都要跟column有對應上
 
-taxon_mpa<-read.xlsx(xlsxFile = "C:/Users/TUNG'S LAB/Desktop/taxon_mpa.xlsx",sheet=1,colNames = T)
+taxon_mpa<-read.xlsx(xlsxFile = "C:/Users/USER/Desktop/lab/實驗/Metagenomic in DWDS/DATA/Taxa/taxon_adjust_mpa.xlsx",sheet=1,colNames = T)
 
 #Phyla relative abundance table
 phyla_table<-taxon_mpa%>% 
   filter(is.na(Class)&is.na(Order)&is.na(Family)&is.na(Genus)&is.na(Species))%>%
   filter(!is.na(Phyla))
-phyla_table[,8:9]<-apply(phyla_table[,8:9],2,function(x) x/sum(x))
-write.xlsx(phyla_table,"C:/Users/TUNG'S LAB/Desktop/phyla.xlsx")
+phyla_table[,8:22]<-apply(phyla_table[,8:22],2,function(x) x/sum(x))
+write.xlsx(phyla_table,"C:/Users/USER/Desktop/phyla_rel_table.xlsx")
 
 #class relative abundance table
 class_table<-taxon_mpa%>%  
   filter(is.na(Order)&is.na(Family)&is.na(Genus)&is.na(Species))%>%
   filter(!is.na(Class))
-class_table[,8:9]<-apply(class_table[,8:9],2,function(x) x/sum(x))
-write.xlsx(class_table,"C:/Users/TUNG'S LAB/Desktop/class.xlsx")
+class_table[,8:22]<-apply(class_table[,8:22],2,function(x) x/sum(x))
+write.xlsx(class_table,"C:/Users/USER/Desktop/class_rel_table.xlsx")
 
 #order relative abundance table
 order_table<-taxon_mpa%>%  
   filter(is.na(Family)&is.na(Genus)&is.na(Species))%>%
   filter(!is.na(Order))
-order_table[,8:9]<-apply(order_table[,8:9],2,function(x) x/sum(x))
-write.xlsx(order_table,"C:/Users/TUNG'S LAB/Desktop/order.xlsx")
+order_table[,8:22]<-apply(order_table[,8:22],2,function(x) x/sum(x))
+write.xlsx(order_table,"C:/Users/USER/Desktop/order_rel_table.xlsx")
 
 #family relative abundance table
 fmaily_table<-taxon_mpa%>%  
   filter(is.na(Genus)&is.na(Species))%>%
   filter(!is.na(Family))
-order_table[,8:9]<-apply(order_table[,8:9],2,function(x) x/sum(x))
-write.xlsx(order_table,"C:/Users/TUNG'S LAB/Desktop/family.xlsx")
+fmaily_table[,8:22]<-apply(fmaily_table[,8:22],2,function(x) x/sum(x))
+write.xlsx(fmaily_table,"C:/Users/USER/Desktop/family_rel_table.xlsx")
 
 #genus relative abundance table
 genus_table<-taxon_mpa%>%  
   filter(is.na(Species))%>%
   filter(!is.na(Genus))
-genus_table[,8:9]<-apply(genus_table[,8:9],2,function(x) x/sum(x))
-write.xlsx(order_table,"C:/Users/TUNG'S LAB/Desktop/genus.xlsx")
+genus_table[,8:22]<-apply(genus_table[,8:22],2,function(x) x/sum(x))
+write.xlsx(genus_table,"C:/Users/USER/Desktop/genus_rel_table.xlsx")
 
 #species relative abundance table
 species_table<-taxon_mpa%>%  
   filter(!is.na(Species))
-species_table[,8:9]<-apply(species_table[,8:9],2,function(x) x/sum(x))
-write.xlsx(order_table,"C:/Users/TUNG'S LAB/Desktop/species.xlsx")
+species_table[,8:22]<-apply(species_table[,8:22],2,function(x) x/sum(x))
+write.xlsx(species_table,"C:/Users/USER/Desktop/species_rel_table.xlsx")
 
 
--- Plot/PCOA_adonistest.R
+++ Plot/PCOA_adonistest.R
@@ -3,8 +3,8 @@ library(ecodist)
 library(ggplot2)
 library(openxlsx)
 library(RColorBrewer)
-dbpata<-read.xlsx("C:/Users/USER/Desktop/lab/台水計畫/DATA/NGS/兩次結果混和/兩次混和 relabundance table/ASVs_mix.xlsx",sheet=1,rowNames=T,colNames=T,sep.names=" ")
-groupata<-read.xlsx("C:/Users/USER/Desktop/lab/台水計畫/DATA/NGS/兩次結果混和/兩次混和 relabundance table/group 兩次混和.xlsx",sheet=1,rowNames=T,colNames=T,sep.names=" ")
+dbpata<-read.xlsx("C:/Users/USER/Desktop/lab/實驗/Metagenomic in DWDS/DATA/Taxa/rel_abundance/species_rel_table.xlsx",sheet=2,rowNames=T,colNames=T,sep.names=" ")
+groupata<-read.xlsx("C:/Users/USER/Desktop/lab/實驗/Metagenomic in DWDS/DATA/Taxa/groupdata.xlsx",sheet=1,rowNames=T,colNames=T,sep.names=" ")
 dbpata <-as.data.frame(t(dbpata))
 dbpata <- decostand(dbpata, method = 'hellinger')
 family_bray<-vegdist(dbpata, method="bray")
@@ -18,17 +18,19 @@ points <-cbind(points, groupata[match(rownames(points), rownames(groupata)), ])
 #用Adonis 來檢定組間差異是不是顯著的
 x<-adonis(dbpata~location,data = groupata)
 x$aov.tab
-ggplot(points, aes(x=Dim1, y=Dim2,colour=plant))+
+points$location<-factor(points$location,levels=c('Raw', 'Finished', 'Upstream','Midstream','Downstream'))
+ggplot(points, aes(x=Dim1, y=Dim2,colour=location))+
   theme_bw()+
-  geom_point(size=4)+geom_text(aes(label=sample_ID),size=3,color="black",)+scale_color_manual(values=color)+#geom_hline(yintercept=0)+ geom_vline(xintercept=0)
-  theme(panel.grid=element_blank())+
+  geom_point(size=3)+#geom_text(aes(label=sample_ID),size=3,color="black",)+
+  scale_color_manual(values = color)+geom_hline(yintercept=0)+ geom_vline(xintercept=0)+theme(panel.grid=element_blank())+
   labs(x=paste("PCoA 1 (", format(100 * eig[1] / sum(eig), digits=4), "%)", sep=""),
        y=paste("PCoA 2 (", format(100 * eig[2] / sum(eig), digits=4), "%)", sep=""),
-       title="bray_curtis PCoA")
+       title="Bray_curtis PCoA")+theme(axis.title = element_text(size=12),legend.title= element_text(size=12),legend.text = element_text(size=12))
 RColorBrewer::display.brewer.all()
 display.brewer.pal(n=12,name="Set3")
 brewer.pal(n=12,name="Set3")
-colora<-c("#BC80BD","#CCEBC5","#FFED6F")
+color<-c("#FB8072","#BEBADA","#80B1D3","#FDB462","#B3DE69")
 #if you use the Rcolor brewer,你需要利用scale_color_brewer(palette="paletee_name")
 #自選顏色的話用Scale_color_manual(values=color_vector)
 ?scale_color_brewer()
+?theme_bw
