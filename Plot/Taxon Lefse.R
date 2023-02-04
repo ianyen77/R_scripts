1e5a8da65347df8ac1efa1586a55f1bc0cea23a9
ianyen77 <96662135+ianyen77@users.noreply.github.com>
2023-02-04 13:54:24 +0800

    Create Taxon Lefse.R

---
+++ Plot/Taxon Lefse.R
@@ -0,0 +1,39 @@
+library(ggplot2)
+library(openxlsx)
+library(ggfortify)
+library(hrbrthemes)
+library(tidyverse)
+library(RColorBrewer)
+#針對mapoutput進行一些預處理，並把它變成galaxy指定的格式
+dbpata<-read.xlsx("C:/Users/USER/Desktop/kraken2 mpaout for lefse.xlsx",sheet=3,rowNames=F,colNames=T,sep.names=" ")
+rownames(dbpata)<-dbpata$`#Classification`
+dbpata$`#Classification`<-gsub(pattern = "k__",replacement = "",x=dbpata$`#Classification`)
+dbpata$`#Classification`<-gsub(pattern = "p__",replacement = "",x=dbpata$`#Classification`)
+dbpata$`#Classification`<-gsub(pattern = "c__",replacement = "",x=dbpata$`#Classification`)
+dbpata$`#Classification`<-gsub(pattern = "o__",replacement = "",x=dbpata$`#Classification`)
+dbpata$`#Classification`<-gsub(pattern = "f__",replacement = "",x=dbpata$`#Classification`)
+dbpata$`#Classification`<-gsub(pattern = "g__",replacement = "",x=dbpata$`#Classification`)
+dbpata$`#Classification`<-gsub(pattern = "s__",replacement = "",x=dbpata$`#Classification`)
+id<-colnames(dbpata)
+id[1]<-"id"
+dbpata<-rbind(id,dbpata)
+Location<-c("Location","Raw","Raw","Raw","Finished","Finished","Finished","Upstream","Upstream","Upstream","Midstream","Midstream","Midstream","Downstream","Downstream","Downstream")
+dbpata<-rbind(Location,dbpata)
+write.xlsx(dbpata,"C:/Users/USER/Desktop/taxon.lefse.xlsx")
+#以上是lefse 的preprocress，之後須到galaxy上跑，跑完再載下來畫圖
+
+#載入分析結果，繪圖(多組group,雙組可再參考學長的腳本)
+gal_lda <- read.xlsx("C:/Users/USER/Desktop/活頁簿1.xlsx",sheet=1,colNames=F)
+colnames(gal_lda) <-  c("TAX","LogMaxMean","Location","LDA","pValue","subtypenum")
+gal_lda<-gal_lda%>%
+  separate(TAX,into=c("type","subtype"),sep ="__")
+#gsub(pattern = "Bacteria_",replacement = "")
+#gal_lda_s$TAX <- str_replace(gal_lda_s$TAX,".{1,}s__","")
+gal_lda$Location <- factor(gal_lda$Location, levels = c("Downstream","Midstream","Upstream","Finished","Raw"))
+gal_lda<-gal_lda[-(which(is.na(gal_lda$LDA))),]
+gal_lda%>%
+  group_by(Location)%>% 
+  mutate(subtype= fct_reorder(subtype, LDA,.desc=F))%>%
+  ggplot()+geom_col(aes(x=LDA, y = subtype,fill = fct_rev(Location)))+
+  scale_fill_discrete(name = "Location")+scale_fill_brewer("Location",palette = "Set3")+
+  guides(fill = guide_legend(label.hjust = 0))+labs(x=expression(LDA~Score~(log[10])),y="")+theme_bw()+theme(axis.title = element_text(size=13),axis.text =element_text(size=12.5)  ,legend.title= element_text(size=12),legend.text = element_text(size=12))
