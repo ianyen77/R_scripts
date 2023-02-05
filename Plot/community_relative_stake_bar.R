cf86ef227e1dd17e1421233e6ee94e82289e1af2
ianyen77 <96662135+ianyen77@users.noreply.github.com>
2023-02-05 20:11:21 +0800

    Update community_relative_stake_bar.R

--- Plot/community_relative_stake_bar.R
+++ Plot/community_relative_stake_bar.R
@@ -1,21 +1,24 @@
 library("openxlsx")
 library("ggplot2")
-library("vegan")
 library("tidyverse")
-data<-read.csv("C:/Users/USER/Desktop/rel_abundance_table/phyla.csv",header=T)
-rownames(data) <- data$X
-data$X<-NULL
+data<-read.xlsx("C:/Users/USER/Desktop/lab/實驗/Metagenomic in DWDS/DATA/Taxa/rel_abundance/phyla_rel_table.xlsx",sheet=1)
+data<-data[,-(3:7)]
+data<-data[,-1]
+data$Phyla<-gsub(pattern = "p__",replacement = "",x=data$Phyla)
+rownames(data)<-data$Phyla
+data$Phyla<-NULL
 data[data<0.05]<-0
 data_sum<-apply(data,2,sum)
 dd<-1-data_sum
 data2<-rbind(data,dd)
-rownames(data2)[35]<-"other; Others"
+rownames(data2)[nrow(data2)]<-"Others"
 data3<-data2[apply(data2, 1, function(x) !all(x==0)),]
 data3$phyla<-rownames(data3)
+data3$phyla<-factor(data3$phyla,levels = c("Actinobacteria","Firmicutes","Planctomycetota","Proteobacteria","Others"))
 rownames(data3)<-NULL
 data4<-gather(data =data3, key ="xx",value = "yf",1:15)
-data4<-separate(data4,phyla,into=c("domain","phyla"),sep=";")
 data5<-data4[-which(data4$yf==0),]
 ggplot(data5)+
   geom_bar(aes(x=xx,y=yf,fill=phyla),color='black',stat="identity")+
-  labs(x=NULL,y=NULL)
+  scale_fill_brewer(palette = "Set3")+theme_bw()+
+  labs(x="Sample",y="Relative abundance")+theme(axis.title = element_text(size=12.5),legend.title= element_text(size=12.5),legend.text = element_text(size=12.5))
