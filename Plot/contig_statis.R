7e7ce38e64720ba4bafc1b2893064f4c59fbd100
ianyen77 <96662135+ianyen77@users.noreply.github.com>
2023-04-02 19:57:29 +0800

    plasflow scripts

--- Plot/contig_statis.R
+++ Plot/contig_statis.R
@@ -2,7 +2,7 @@ library(openxlsx)
 library(tidyverse)
 library(pheatmap)
 library(RColorBrewer)
-data_contig<-read.xlsx("C:/Users/USER/Desktop/ARC_analysis/ARC_phyla_整理.xlsx",sheet=1)
+data_contig<-read.xlsx("C:/Users/USER/Desktop/lab/實驗/Metagenomic in DWDS/DATA/newDATA/ARC_analysis/ARC_phyla_整理.xlsx",sheet=1)
 data_contig$contig_phyla[is.na(data_contig$contig_phyla)]<-"Unclassified"
 data_contig$contig_phyla[data_contig$contig_phyla == "Bacteria"]<-"Unclassified"
 data_stat<-data_contig%>%
