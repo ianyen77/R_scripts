6cc3ff8d93da1d3f72fe5cdd062b514b8b4ffa34
ianyen77 <96662135+ianyen77@users.noreply.github.com>
2023-04-05 21:38:47 +0800

    update

--- Plot/contig_statis.R
+++ Plot/contig_statis.R
@@ -6,6 +6,51 @@ library(ggsankey)
 data_contig<-read.xlsx("C:/Users/USER/Desktop/lab/實驗/Metagenomic in DWDS/DATA/newDATA/ARC_analysis/ARC_phyla_整理.xlsx",sheet=1)
 data_contig$contig_phyla[is.na(data_contig$contig_phyla)]<-"Unclassified"
 data_contig$contig_phyla[data_contig$contig_phyla == "Bacteria"]<-"Unclassified"
+##ARG type percentage----------------------------------
+data_stat<-data_contig%>%
+  group_by(type)%>%
+  count()%>%
+  ungroup()%>%
+  mutate(sum=sum(n))%>%
+  group_by(type)%>%
+  mutate(cover=n/sum) 
+
+#ARG host percentage--------------------------------
+data_stat<-data_contig%>%
+  group_by(contig_phyla)%>%
+  count()%>%
+  ungroup()%>%
+  mutate(sum=sum(n))%>%
+  group_by(contig_phyla)%>%
+  mutate(cover=n/sum) 
+#ARG sample host perecntage----------------------
+data_stat<-data_contig%>%
+  group_by(Sample,contig_phyla)%>%
+  count()%>%
+  ungroup()%>%
+  group_by(Sample)%>%
+  mutate(n_S=sum(n))%>%
+  mutate(type_ACC_percent=n/n_S)
+convert_column_names <- function(data, column) {
+  column_names <- c("T1", "T2", "T3", "T4", "T5")
+  new_names <- c("Raw", "Finished", "Upstream", "Midstream", "Downstream")
+  
+  for (i in seq_along(column_names)) {
+    data[[column]][data[[column]] == column_names[i]] <- new_names[i]
+  }
+  
+  return(data)
+}
+data_stat <- convert_column_names(data_stat, "Sample")
+data_stat$Sample<-factor(data_stat$Sample,levels = c("Raw","Finished","Upstream","Midstream","Downstream"))
+color<-c( "#8DD3C7","#FFFFB3" ,"#FB8072","#BEBADA","#B3DE69","#80B1D3" , "#FDB462","#FCCDE5" ,"#D9D9D9","#BEBADA" ,"#CCEBC5" ,"#FFED6F")
+color1<-hcl.colors(7,"sunset")
+#relative abundance(ARC number)
+ggplot(data_stat)+geom_bar(aes(x=Sample,y=type_ACC_percent,fill=contig_phyla,color=contig_phyla),alpha=0.7,stat="identity",width=0.8)+
+  scale_fill_manual("ARC Phyla",values = color1)+scale_color_manual("ARC Phyla",values = color1)+theme_bw()+labs(x="Location",y="Relative abundance")+
+  theme(axis.title = element_text(size=13),axis.text = element_text(size=12),legend.title= element_text(size=12),legend.text = element_text(size=12))
+
+
 
 ##ARG orf type coverage---------------------------------------------------------------
 data_stat<-data_contig%>%
@@ -33,6 +78,7 @@ data_stat$type[data_stat$type=="Macrolide-lincosamide-streptogramin"]<-"MLS"
 data_stat$Sample<-factor(data_stat$Sample,levels = c("Raw","Finished","Upstream","Midstream","Downstream"))
 color1<-c( "#FFFFB3" ,"#BC80BD" , "#FB8072" ,"#80B1D3" ,"#FDB462" ,"#B3DE69" ,"#FCCDE5" ,"#D9D9D9","#BEBADA" ,"#CCEBC5" ,"#FFED6F")
 color<- hcl.colors(14, "Sunset")
+
 ggplot(data_stat)+geom_bar(aes(x=Sample,y=orf_coverage,fill=type,color=type),stat="identity",alpha=0.7,width = 0.8)+
   scale_fill_manual("Location",values = color)+scale_color_manual("Location",values = color)+theme_bw()+labs(x="ARG Type",y="ARC coverage (x/GB)")+
   theme(axis.title = element_text(size=13),axis.text = element_text(size=12),legend.title= element_text(size=12),legend.text = element_text(size=12))
