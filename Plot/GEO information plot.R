f1bf2ab4ccdaa2f0115f21434bbaaf6eda1358b4
ianyen77 <96662135+ianyen77@users.noreply.github.com>
2023-04-19 14:28:21 +0800

    OK

--- Plot/GEO information plot.R
+++ Plot/GEO information plot.R
@@ -6,20 +6,22 @@ library("tidyverse")
 library("openxlsx")
 library("stringr")
 library("RColorBrewer")
-sample<-read.xlsx(xlsxFile = "C:/Users/USER/Desktop/lab/實驗/Metagenomic in DWDS/Plot/採樣位置/採樣點地理資訊.xlsx",sheet=3)
-trek_df <- trek("23.125018652096625, 121.39324881712558", "22.875292748459103, 121.22908696386816", structure = "route")
+sample<-read.xlsx(xlsxFile = "C:/Users/USER/Desktop/lab/實驗/Metagenomic in DWDS/Plot/採樣位置/採樣點地理資訊.xlsx",sheet=4)
 #for safety reason we need to assign google api key each time
-register_googlye(key = "")
+register_google(key = "")
+trek_df <- trek("23.125018652096625, 121.39324881712558", "22.875292748459103, 121.22908696386816", structure = "route")
 TaitoungMap <- get_googlemap(
   center  = c(lon=121.31154590744264,lat=22.998088043256093), 
   zoom = 11, maptype = 'roadmap')
 #draw a plot
-display.brewer.pal(name="Spectral",n=11)
-brewer.pal(name="Spectral",n=11)
-cols=c("#66C2A5","#3288BD")
 
+cols=c("#3288BD" ,"#F8766D","#00BFC4")
+shape=c(15,16,17)
 TaitoungMappoint<- ggmap(TaitoungMap)+ 
-  geom_point(data=sample,aes(x=lon, y=lat,shape=type,color=type),size=4.5)+geom_text(data=sample,label=sample$sampeltype,family="serif",size=6,vjust =1.6,hjust=-0.1)+theme_minimal()+theme (legend.position= "none") 
+  geom_point(data=sample,aes(x=lon, y=lat,shape=type,color=type),size=3)+geom_text(data=sample,label=sample$sampeltype,family="serif",size=6,vjust =1.6,hjust=-0.1)+theme_minimal()+theme (legend.position= "none")+scale_color_manual(values=cols)+scale_shape_manual(values = shape) 
   #+geom_path(aes(x = lon, y = lat),  colour = "#66a182",size = 2.3, alpha = .7,data = trek_df, lineend = "square")
 TaitoungMappoint
-?theme_minimal
+
+library(scales)
+hex <- hue_pal()(3)
+show_col(hex)
